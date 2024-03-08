682ee35bd3f0cabcd802cfba5ba8644dbc3cf2cb
Vaibhav <vaibhav.lakhera@razorpay.com>
2024-03-08 18:02:23 +0530

    feat: add validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, length: {minimum: 5, maximum: 256}, format: {with: /\A[a-zA-Z 0-9!@#$^&*()_-]*\z/, message: 'must be alphanumeric and !@#$^&*()_- only'}
+  validates :description, length: {minimum: 10}, format: {with: /\A[a-zA-Z 0-9!@#$^&*()_-]*\z/, message: 'must be alphanumeric and !@#$^&*()_- only'} 
 end
