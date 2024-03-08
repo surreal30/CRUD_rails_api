ae761da2fe2c15146f4d5b9c9b995d6b3fbabfa5
Vaibhav <vaibhav.lakhera@razorpay.com>
2024-03-08 18:11:05 +0530

    feat: add validation for comment body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :body, length: {minimum: 4}, format: {with: /\A[a-zA-Z 0-9!@#$^&*()_-]*\z/, message: 'must be alphanumeric and !@#$^&*()_- only'} 
 end
