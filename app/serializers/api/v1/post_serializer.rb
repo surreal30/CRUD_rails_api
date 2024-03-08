300057bea3a80bfc9a45dcd6c4c3cfb15706ea00
Vaibhav <vaibhav.lakhera@razorpay.com>
2024-03-08 19:40:33 +0530

    feat: show first 5 comments and likes

--- app/serializers/api/v1/post_serializer.rb
+++ app/serializers/api/v1/post_serializer.rb
@@ -1,3 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
+  has_many :comments do
+    @object.comments.limit(5)
+  end
+  has_many :likes do
+    @object.likes.limit(5)
+  end
+
   attributes :id, :title, :description, :slug, :likes_count, :comments_count, :user_id, :comments, :likes
 end
