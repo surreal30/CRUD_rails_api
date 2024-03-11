792559176b93bb38489c4efbd7a22d7e35f7adb4
Vaibhav <vaibhav.lakhera@razorpay.com>
2024-03-11 10:57:27 +0530

    feat: add error_messages

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -7,12 +7,12 @@ class Api::V1::CommentsController < ApplicationController
   def index
     comments = Comment.where(post_id: params[:post_id])
 
-    render json: comments
+    render json: comments, status: 200
   end
 
   # GET /comments/1
   def show
-    render json: @comment 
+    render json: @comment, status: 200
   end
 
   # POST /comments
@@ -23,7 +23,7 @@ class Api::V1::CommentsController < ApplicationController
     if comment.save 
       render json: {data: comment}, status: 200
     else
-      render status: :unprocessable_entity
+      render json: {error_message: comment.errors, error_code: 422}, status: 422
     end
   end
 
@@ -36,7 +36,7 @@ class Api::V1::CommentsController < ApplicationController
         render json: @comment.errors, status: :unprocessable_entity
       end
     else
-      render status: 401
+      render json: {error_message: "Unauthorized user", error_code: 401}, status: 401
     end
   end
 
@@ -45,18 +45,19 @@ class Api::V1::CommentsController < ApplicationController
     if @comment.user_id == @user.id
       @comment.destroy!
     else
-      render status: 401
+      render json: {error_message: "Unauthorized user", error_code: 401}, status: 401
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find_by(post_id: params[:post_id], id: params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.permit(:body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find_by(post_id: params[:post_id], id: params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def comment_params
+    params.permit(:body)
+  end
 end
